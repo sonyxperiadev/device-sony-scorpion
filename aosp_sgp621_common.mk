73f2852f28f643715bc44292af7fab382680cb9d
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-02-01 19:40:23 +0100

    add common git
    include common git into Android Build
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- aosp_sgp621_common.mk
+++ aosp_sgp621_common.mk
@@ -19,6 +19,7 @@ PRODUCT_COPY_FILES += \
     device/sony/scorpion/rootdir/fstab.shinano:root/fstab.shinano
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, device/sony/common/common.mk)
 $(call inherit-product, device/sony/shinano/device.mk)
 $(call inherit-product, vendor/sony/scorpion/scorpion-vendor.mk)
 $(call inherit-product, frameworks/native/build/tablet-7in-xhdpi-2048-dalvik-heap.mk)
